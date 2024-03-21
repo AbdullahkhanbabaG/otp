ab73d1dcedd259989c7e4f6c6e69e7979fc108ae
AbdullahkhanbabaG <164285326+AbdullahkhanbabaG@users.noreply.github.com>
2024-03-20 19:34:55 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let generateOTP = () => {
+    //Define the length of the OTP
+    const otpLength = 4;
+  
+    // Generate a random numeric OTP with exactly 6 digits
+    const otp = Math.floor(1000 + Math.random() * 9000);
+  
+    //Display the generated OTP
+    document.getElementById("otpDisplay").innerText = `${otp}`;
+  };
+  
+  document.getElementById("generateBtn").addEventListener("click", generateOTP);
+  window.addEventListener("load", generateOTP);
